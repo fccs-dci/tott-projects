f51a095561574be4746891553e5315c78ca0dad8
KL Tang <15926391+kltng@users.noreply.github.com>
2023-03-09 07:47:35 -0500

    fix site.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fccs-dci.github.io/',
-    base: '/tott-projects'
+    site: 'https://fccs-dci.github.io/tott-projects/',
+    // base: '/tott-projects'
 });
