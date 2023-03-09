454e25caef08c527d02314eae042da42f97a6649
KL Tang <15926391+kltng@users.noreply.github.com>
2023-03-09 07:36:33 -0500

    add site and base.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astro.build',
+    site: 'https://fccs-dci.github.io/',
+    base: '/tott-projects'
 });
